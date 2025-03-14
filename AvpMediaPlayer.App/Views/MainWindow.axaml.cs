479a6f1baacc76959abacf1b612d365b749f8e5d
alex.panarin.69 <alex.panarin.69@mail.ru>
2025-03-14 11:20:28 +0300

    Add project files.

---
+++ AvpMediaPlayer.App/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AvpMediaPlayer.App.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
